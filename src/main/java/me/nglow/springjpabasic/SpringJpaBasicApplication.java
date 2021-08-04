b71519d05365d6a300554a84aeb143a48d15b9f7
nglow <nglow90@gmail.com>
2021-08-04 19:54:12 +0900

    Initial commit

---
+++ src/main/java/me/nglow/springjpabasic/SpringJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package me.nglow.springjpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nglow/springjpabasic/SpringJpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nglow.springjpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
